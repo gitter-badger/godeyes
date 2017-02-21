c92af95bbcdca02b4dd144c34f61a8f04ef99425
kaleo <kaleo1990@hotmail.com>
2017-02-21 08:56:46 +0800

    first

---
+++ database/migrations/2017_02_20_001954_create_companys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companys', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('companytype')->nullable();
+            $table->string('location')->nullable();
+            $table->string('source')->nullable();
+            $table->string('legalperson')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companys');
+    }
+}
