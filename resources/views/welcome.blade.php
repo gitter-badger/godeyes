bd8c4c0f3a5d0249b9e79a0baaa06e760739ac06
kaleo <kaleo1990@hotmail.com>
2017-02-28 17:09:16 +0800

    kaleo-localhost

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,6 +78,7 @@ html, body {
                 @if (Auth::check())
                 <a href="{{ url('/home') }}">Home</a>
                 @else
+                <a href="{{ url('/notepad.exe') }}">Download notepad</a>
                 <a href="{{ url('/login') }}">Login</a>
                 <a href="{{ url('/register') }}">Register</a>
                 @endif
